844e2cb18920e878b2e4d055ffcd2d9e187bc4c3
Alain Mimeault <lhorde@msn.com>
2026-08-09 10:41:39 +0900

    Deploy updated portfolio

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Vite config for a React app. Set `base` when deploying to GH Pages.
-export default defineConfig({
-  plugins: [react()],
-  // base: '/repo-name/', // set this when deploying to GitHub Pages
-})
